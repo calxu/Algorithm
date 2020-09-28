ea61c95b8ec76cbbd8e64de5b1f7f68b737a4d71
Calvin,Xu <csxucaixu@gmail.com>
2020-09-29 05:17:28 +0800

    update 013.py

--- 013.py
+++ 013.py
@@ -1,37 +1,59 @@
-# 回溯法-八皇后问题
+#!/usr/bin/python3
+# coding:utf-8
+
+# 回溯法-八皇后问题所有可能
 
 
-n = 8         # 定义八个皇后
-X = [0] * n   # 表示列 
 
+def place(row, column):
+    """
+        限界函数；
+        如果一个皇后能放在第row行和column[row]列，则返回true；否则返回false。
+        前row行已放置皇后
+    """
 
-# 限界函数
-# 如果一个皇后能放在第k行和x(k)列,则返回true;否则返回false.
-# X是全程数组,进入此过程时已置了k个值,ABS(r)过程返回r的绝对值
-def PLACE(k):
     i = 0
-    while i < k:
-        if X[i] == X[k] or abs(X[i]-X[k]) == abs(i-k):
+
+    while i < row:
+        # 不允许同一列；不允许对角线
+        if column[i] == column[row] or abs(column[i]-column[row]) == abs(i-row):
             return False
+
         i += 1
 
     return True
 
 
-# n皇后所有解的递归算法
-def NQ(k):
-    X[k] = 0
 
-    while X[k]<n:
-        if PLACE(k):
-            if k == n-1:
-                print(X[0], X[1], X[2], X[3], X[4], X[5], X[6], X[7])
+def n_queen(row, column, n):
+    """
+        n皇后所有解
+    """
+    # 初始化放置第一列
+    column[row] = 0
+
+    while column[row] < n:
+        if place(row, column):
+            # 最后一行放置成功
+            if row == n - 1:
+                print( column )
             else:
-                NQ(k+1)
+                # 下一行
+                n_queen( row + 1, column, n )
+        
+        # 下一列
+        column[row] = column[row] + 1
 
-        X[k] = X[k] + 1
 
 
-# 测试代码
 if __name__ == '__main__':
-    NQ(0)
+    # 棋盘的大小
+    n = 8
+    
+    # 棋盘上的行
+    row = 0 
+
+    # 初始化，第 row 行，第 column[row] 列放置皇后
+    column = [0] * n
+
+    n_queen(row, column, n)
